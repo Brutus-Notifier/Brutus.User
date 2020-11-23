748b3d1eb9aa091fea39d2da0ba5094301f2eddf
Ihor Hadzera <ihor.hadzera@gmail.com>
2020-11-23 19:50:07 +0100

    added migations

---
+++ Brutus.User/Migrations/20201123163231_Initial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Brutus.User.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInvitations",
+                columns: table => new
+                {
+                    Id = table.Column<byte[]>(nullable: false),
+                    UserId = table.Column<byte[]>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInvitations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserInvitations");
+        }
+    }
+}
